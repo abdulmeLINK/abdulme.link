80d19c2600a1c840372bf7ebb0993c92722d9aae
Abdulmelik Saylan <47142956+abdulmeLINK@users.noreply.github.com>
2024-01-08 05:47:11 +0300

    use laravel asset url generator

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') - Abdulmelik Saylan</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link href="{{ asset('resources/css/app.css') }}" media="all" rel="stylesheet" type="text/css" />
+    <script type="module" src="{{ asset('resources/js/app.js') }}"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
