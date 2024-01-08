f79e4aa250843828544c7b6e5227a24c1f96b054
Abdulmelik Saylan <47142956+abdulmeLINK@users.noreply.github.com>
2024-01-08 04:44:45 +0300

    set auto exec

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ export default defineConfig({
     plugins: [  
         laravel({
             server: {
+                https:true,
+                host: 'abdulme.link',
                port: 5174
+
             },
             input: [ 'resources/js/app.js'],
             refresh: true,
