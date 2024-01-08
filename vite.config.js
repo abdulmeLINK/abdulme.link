2b16768115a2d8f5e9883dae1d62a3b28f25a468
Abdulmelik Saylan <47142956+abdulmeLINK@users.noreply.github.com>
2024-01-08 04:36:31 +0300

    change port to 5174

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [  
         laravel({
+            host: {
+               port: 5174
+            },
             input: [ 'resources/js/app.js'],
             refresh: true,
         }),
